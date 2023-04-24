becadec86f4a307928361907b9790cff3250f2ec
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-24 09:56:31 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /**
+ * is_alpha - checks if a character is alphabetic
+ * @c: the character to check
+ * Return: 1 if the character is alphabetic, 0 otherwise
+ */
+
+int is_alpha(char c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+static format_specifier format_specifiers[] = {
+	{"c", print_char},
+};
+
+/**
  * _printf - implementation of printf
  *
  * @format: string character
@@ -11,5 +26,36 @@
 
 int _printf(const char *format, ...)
 {
-	
+	int count = 0, i;
+	format_t f = {0, -1, -1, -1, -1};
+	va_list args;
+
+	/* Note: printf segfaults if format is NULL */
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			f = get_format(&format);
+			if (f.flags == NULL)
+				return (-1);
+
+			for (i = 0; format_specifiers[i].specifier; ++i)
+				if (f.specifier == *format_specifiers[i].specifier)
+				{
+					format_specifiers[i].function(args, f, &count);
+					format++;
+					break;
+				}
+			free(f.flags);
+		}
+		else
+			_putchar(*format++, &count);
+	}
+	va_end(args);
+	return (count);
 }
--- main.h
+++ main.h
@@ -6,3 +6,6 @@
 void print_char(va_list, format_t, void *);
 void _putchar(char, void *);
 int _printf(const char *format, ...);
+int is_alpha(char c);
+
+#endif
